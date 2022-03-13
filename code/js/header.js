f3c2760f69f4989d197112b44b578896abedabe9
Sacs Estoul <96799620+SacsEstoul@users.noreply.github.com>
2022-03-13 15:29:13 -0700

    Update header.js

--- code/js/header.js
+++ code/js/header.js
@@ -6,39 +6,38 @@ class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <style>
-        nav {
-          height: 40px;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          background-color:  #0a0a23;
-        }
+    .bottom-one {
+         margin-bottom: 1cm;
+         overflow: hidden; 
+    }
 
-        ul {
-          padding: 0;
-        }
+    .bottom-two {
+         margin-bottom: 2cm;
+         overflow: hidden; 	
+    }
+    </style>
+    
+    <nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+        <div class="container">
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
         
-        a {
-          font-weight: 700;
-          margin: 0 25px;
-          color: #fff;
-          text-decoration: none;
-        }
+        <ul class="nav navbar-nav"> 
+	          <li><a href="#">&#127968; Accueil</a></li>
+	          <li><a href="barre_en_haut/nouveautes.html">&#128171; NOUVEAUTÉS!</a></li>  		  
+            <li><a href="mailto:sacs.estoul@gmail.com">&#x1F4E7; Adresse Courriel</a></li>	  		  		  		  
+            <li><a href="barre_en_haut/petite_histoire.html">&#128092; La petite histoire</a></li>
+	          <li><a href="barre_en_haut/faq.html">&#10067; FAQ</a></li>	
+            <li><a href="barre_en_haut/rabais.html">&#x2702; Par ici les rabais!</a></li>		  	  
+            <div class="bottom-two"></div>
+		  
+	          <a style="font-family:Didot;font-size:48px;color:#5F9EA0" href="#">&nbsp;Les Sacs ESTOUL</a>  
+	          <div class="bottom-one"></div>   
+		  
+        </ul>
         
-        a:hover {
-          padding-bottom: 5px;
-          box-shadow: inset 0 -2px 0 0 #fff;
-        }
-      </style>
-      <header>
-        <nav>
-          <ul>
-            <li><a href="about.html">About</a></li>
-            <li><a href="work.html">Work</a></li>
-            <li><a href="contact.html">Contact</a></li>
-          </ul>
-        </nav>
-      </header>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container -->
+    </nav>
     `;
   }
 }
