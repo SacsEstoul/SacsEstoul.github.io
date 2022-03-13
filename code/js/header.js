6cfe4ad39a350fb4f2b8fc33072a09ac29e7a568
Sacs Estoul <96799620+SacsEstoul@users.noreply.github.com>
2022-03-13 15:23:21 -0700

    Update header.js

--- code/js/header.js
+++ code/js/header.js
@@ -2,170 +2,45 @@ class Header extends HTMLElement {
   constructor() {
     super();
   }
-}
 
+  connectedCallback() {
+    this.innerHTML = `
+      <style>
+        nav {
+          height: 40px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background-color:  #0a0a23;
+        }
 
-connectedCallback() {
-  this.innerHTML = `
-        <style>
-	#body {
-		margin-top: 50px; /* 50px is the height of the navbar - change this if the navbarn height changes */
-		overflow: hidden; 
-	}
-
-	.portfolio-item {
-		margin-bottom: 25px;
-	}
-
-	#footer {
-		margin: 50px 0;
-		overflow: hidden; 
-	}
-
-	.bottom-tiny {
-	     margin-bottom: 0.25cm;
-	     overflow: hidden; 
-	}
-
-	.bottom-sm {
-	     margin-bottom: 0.5cm;
-	     overflow: hidden; 	
-	}
-
-
-	.bottom-one {
-	     margin-bottom: 1cm;
-	     overflow: hidden; 
-	}
-
-	.bottom-two {
-	     margin-bottom: 2cm;
-	     overflow: hidden; 	
-	}
-
-	.bottom-three {
-	     margin-bottom: 3cm;
-	     overflow: hidden; 	
-	}
-
-
-	.page {
-	  width: 100vw;
-	  height: 100vh;
-	  position: fixed;
-	  top: 0;
-	  left: -100vw;
-	  overflow-y: auto;
-	  z-index: 0;
-	  background-color: hsl(0,0%,100%);
-	}
-
-	.page:target {
-	  left: 0vw;
-	  z-index: 1;
-	}
-
-
-	.logo_topright {
-	  position:absolute;
-	  top:5px;
-	  right:5px;
-	  height: 160px;
-	  width: 160px;	
-	}
-
-
-	.topright {
-	    position: absolute;
-	    right: 0;
-	    top: 0;
-	    display: block;
-	    height: 130px;
-	    width: 130px;
-	    text-indent: -999em;
-	    text-decoration: none;
-	}
-
-	.logo_bottomright {
-	  position:absolute;
-	  bottom:5px;
-	  right:5px;
-	  height: 160px;
-	  width: 160px;	
-	}
-
-
-	.bottomright {
-	    position: absolute;
-	    right: 0;
-	    bottom: 0;
-	    display: block;
-	    height: 130px;
-	    width: 130px;
-	    text-indent: -999em;
-	    text-decoration: none;
-	}
-
-
-	.logo_bottomleft {
-	  position:absolute;
-	  bottom:5px;
-	  left:5px;
-	  height: 160px;
-	  width: 160px;	
-	}
-
-
-	.bottomleft {
-	    position: absolute;
-	    left: 0;
-	    bottom: 0;
-	    display: block;
-	    height: 130px;
-	    width: 130px;
-	    text-indent: -999em;
-	    text-decoration: none;
-	}
-
-
-	.link_border {
-	    border:2px;
-	    border-style:solid;
-	    border-color:#5F9EA0;
-	    padding:0.5em;	
-	}	
-        </style>
-	<header>	    
-	<nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
-	<div class="container">
-
-	<div class="collapse navbar-collapse navbar-ex1-collapse">
-	  <ul class="nav navbar-nav"> 
-		  <li><a href="#">&#127968; Accueil</a></li>
-		  <li><a href="barre_en_haut/nouveautes.html">&#128171; NOUVEAUTÉS!</a></li>  		  
-	          <li><a href="mailto:sacs.estoul@gmail.com">&#x1F4E7; Adresse Courriel</a></li>	  		  		  		  
-	          <li><a href="barre_en_haut/petite_histoire.html">&#128092; La petite histoire</a></li>
-		  <li><a href="barre_en_haut/faq.html">&#10067; FAQ</a></li>	
-	          <li><a href="barre_en_haut/rabais.html">&#x2702; Par ici les rabais!</a></li>		  	  
-	          <div class="bottom-two"></div>
-
-		  <a style="font-family:Didot;font-size:48px;color:#5F9EA0" href="#">&nbsp;Les Sacs ESTOUL</a>  
-		  <div class="bottom-one"></div>   
-
-	  </ul>
-	</div><!-- /.navbar-collapse -->
-	</div><!-- /.container -->
-	</nav>
-	</header>	     
-    
-    
-    
-    
+        ul {
+          padding: 0;
+        }
+        
+        a {
+          font-weight: 700;
+          margin: 0 25px;
+          color: #fff;
+          text-decoration: none;
+        }
+        
+        a:hover {
+          padding-bottom: 5px;
+          box-shadow: inset 0 -2px 0 0 #fff;
+        }
+      </style>
+      <header>
+        <nav>
+          <ul>
+            <li><a href="about.html">About</a></li>
+            <li><a href="work.html">Work</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
     `;
   }
-}    
+}
 
-
-
-// {extends: 'nav'} as 3rd arg
 customElements.define('header-component', Header);
