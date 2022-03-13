0b564a3773da4f6525fae75dc596d69d0563abe0
Sacs Estoul <96799620+SacsEstoul@users.noreply.github.com>
2022-03-13 16:13:00 -0700

    Update top.js

--- code/js/top.js
+++ code/js/top.js
@@ -25,15 +25,8 @@ class Top extends HTMLElement {
         <meta http-equiv="X-UA-Compatible" content="ie=edge">	  
         <meta name="author" content="Estoul">
 
-        <!--  a)  Les 2 lignes suivantes servent à la découverte du site sur Google	   -->
         <meta name="description", content="Collection de Sacs Design Uniques / Handmade Tote Bags">
         <meta name="keywords", content="sacs Estoul, sacs, sacs faits main, sacoche, sacs de sport, sacs uniques, Quebec, Trois-Rivieres, bags, tote bags">	 
-        <!--  a)   -->
-
-        <!--  1) CSS style using bootstrap -->
-        <link href="code/css/bootstrap.css" rel="stylesheet">
-        <!-- Custom CSS for the '3 Col Portfolio' Template -->
-        <link href="code/css/3-col-portfolio.css" rel="stylesheet">
 
     `;
   }
