df17892310baa183a74fbc95bbceb113bf2a9dbb
Sacs Estoul <96799620+SacsEstoul@users.noreply.github.com>
2022-03-13 14:32:26 -0700

    Update header.js

--- code/js/header.js
+++ code/js/header.js
@@ -7,31 +7,6 @@ class Header extends HTMLElement {
 
 connectedCallback() {
   this.innerHTML = `
-    <style>
-      nav {
-        height: 40px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color:  #0a0a23;
-      }
-
-      ul {
-        padding: 0;
-      }
-
-      a {
-        font-weight: 700;
-        margin: 0 25px;
-        color: #fff;
-        text-decoration: none;
-      }
-
-      a:hover {
-        padding-bottom: 5px;
-        box-shadow: inset 0 -2px 0 0 #fff;
-      }
-    </style>
     
   <header>	  
   <!--  b) Les prochaines lignes sont pour la barre d'en haut du site - Nom, contact, descriptions avec liens	 -->	  
