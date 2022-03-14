3d2fd95787dd8b9a391f847accebcf8f89d66abc
Sacs Estoul <96799620+SacsEstoul@users.noreply.github.com>
2022-03-13 22:24:48 -0700

    Update header.js

--- code/js/header.js
+++ code/js/header.js
@@ -48,99 +48,99 @@ customElements.define('hc', Header);
 
 
 
-class Header2 extends HTMLElement {
-  constructor() {
-    super();
-  }
+// class Header2 extends HTMLElement {
+//   constructor() {
+//     super();
+//   }
 
-  connectedCallback() {
-    this.innerHTML = `
+//   connectedCallback() {
+//     this.innerHTML = `
     
-    <style>
-    .bottom-one {
-         margin-bottom: 1cm;
-         overflow: hidden; 
-    }
-    .bottom-two {
-         margin-bottom: 2cm;
-         overflow: hidden; 	
-    }
-    </style>
+//     <style>
+//     .bottom-one {
+//          margin-bottom: 1cm;
+//          overflow: hidden; 
+//     }
+//     .bottom-two {
+//          margin-bottom: 2cm;
+//          overflow: hidden; 	
+//     }
+//     </style>
     
-    <nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
-        <div class="container">
-        <div class="collapse navbar-collapse navbar-ex1-collapse">
+//     <nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+//         <div class="container">
+//         <div class="collapse navbar-collapse navbar-ex1-collapse">
         
-        <ul class="nav navbar-nav"> 
-	    <li><a href="../index.html">&#127968; Accueil</a></li>
-	    <li><a href="../barre_en_haut/nouveautes.html">&#128171; NOUVEAUTÉS!</a></li>  		  
-            <li><a href="mailto:sacs.estoul@gmail.com">&#x1F4E7; Adresse Courriel</a></li>	  		  		  		  
-            <li><a href="../barre_en_haut/petite_histoire.html">&#128092; La petite histoire</a></li>
-	    <li><a href="../barre_en_haut/faq.html">&#10067; FAQ</a></li>	
-            <li><a href="../barre_en_haut/rabais.html">&#x2702; Par ici les rabais!</a></li>
+//         <ul class="nav navbar-nav"> 
+// 	    <li><a href="../index.html">&#127968; Accueil</a></li>
+// 	    <li><a href="../barre_en_haut/nouveautes.html">&#128171; NOUVEAUTÉS!</a></li>  		  
+//             <li><a href="mailto:sacs.estoul@gmail.com">&#x1F4E7; Adresse Courriel</a></li>	  		  		  		  
+//             <li><a href="../barre_en_haut/petite_histoire.html">&#128092; La petite histoire</a></li>
+// 	    <li><a href="../barre_en_haut/faq.html">&#10067; FAQ</a></li>	
+//             <li><a href="../barre_en_haut/rabais.html">&#x2702; Par ici les rabais!</a></li>
 	    
-            <div class="bottom-two"></div>
+//             <div class="bottom-two"></div>
 		  
-	    <a style="font-family:Didot;font-size:48px;color:#5F9EA0" href="../index.html">&nbsp;Les Sacs ESTOUL</a>  
+// 	    <a style="font-family:Didot;font-size:48px;color:#5F9EA0" href="../index.html">&nbsp;Les Sacs ESTOUL</a>  
 	    
-	    <div class="bottom-one"></div>   		  
-        </ul>
+// 	    <div class="bottom-one"></div>   		  
+//         </ul>
         
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container -->
-    </nav>
-    `;
-  }
-}
+//         </div><!-- /.navbar-collapse -->
+//       </div><!-- /.container -->
+//     </nav>
+//     `;
+//   }
+// }
 
-customElements.define('hc2', Header2);
-
-
+// customElements.define('hc2', Header2);
 
 
-class Header3 extends HTMLElement {
-  constructor() {
-    super();
-  }
 
-  connectedCallback() {
-    this.innerHTML = `
+
+// class Header3 extends HTMLElement {
+//   constructor() {
+//     super();
+//   }
+
+//   connectedCallback() {
+//     this.innerHTML = `
     
-    <style>
-    .bottom-one {
-         margin-bottom: 1cm;
-         overflow: hidden; 
-    }
-    .bottom-two {
-         margin-bottom: 2cm;
-         overflow: hidden; 	
-    }
-    </style>
+//     <style>
+//     .bottom-one {
+//          margin-bottom: 1cm;
+//          overflow: hidden; 
+//     }
+//     .bottom-two {
+//          margin-bottom: 2cm;
+//          overflow: hidden; 	
+//     }
+//     </style>
     
-    <nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
-        <div class="container">
-        <div class="collapse navbar-collapse navbar-ex1-collapse">
+//     <nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+//         <div class="container">
+//         <div class="collapse navbar-collapse navbar-ex1-collapse">
         
-        <ul class="nav navbar-nav"> 
-	    <li><a href="../../index.html">&#127968; Accueil</a></li>
-	    <li><a href="../../barre_en_haut/nouveautes.html">&#128171; NOUVEAUTÉS!</a></li>  		  
-            <li><a href="mailto:sacs.estoul@gmail.com">&#x1F4E7; Adresse Courriel</a></li>	  		  		  		  
-            <li><a href="../../barre_en_haut/petite_histoire.html">&#128092; La petite histoire</a></li>
-	    <li><a href="../../barre_en_haut/faq.html">&#10067; FAQ</a></li>	
-            <li><a href="../../barre_en_haut/rabais.html">&#x2702; Par ici les rabais!</a></li>
+//         <ul class="nav navbar-nav"> 
+// 	    <li><a href="../../index.html">&#127968; Accueil</a></li>
+// 	    <li><a href="../../barre_en_haut/nouveautes.html">&#128171; NOUVEAUTÉS!</a></li>  		  
+//             <li><a href="mailto:sacs.estoul@gmail.com">&#x1F4E7; Adresse Courriel</a></li>	  		  		  		  
+//             <li><a href="../../barre_en_haut/petite_histoire.html">&#128092; La petite histoire</a></li>
+// 	    <li><a href="../../barre_en_haut/faq.html">&#10067; FAQ</a></li>	
+//             <li><a href="../../barre_en_haut/rabais.html">&#x2702; Par ici les rabais!</a></li>
 	    
-            <div class="bottom-two"></div>
+//             <div class="bottom-two"></div>
 		  
-	    <a style="font-family:Didot;font-size:48px;color:#5F9EA0" href="../../index.html">&nbsp;Les Sacs ESTOUL</a>  
+// 	    <a style="font-family:Didot;font-size:48px;color:#5F9EA0" href="../../index.html">&nbsp;Les Sacs ESTOUL</a>  
 	    
-	    <div class="bottom-one"></div>   		  
-        </ul>
+// 	    <div class="bottom-one"></div>   		  
+//         </ul>
         
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container -->
-    </nav>
-    `;
-  }
-}
+//         </div><!-- /.navbar-collapse -->
+//       </div><!-- /.container -->
+//     </nav>
+//     `;
+//   }
+// }
 
-customElements.define('hc3', Header3);
+// customElements.define('hc3', Header3);
