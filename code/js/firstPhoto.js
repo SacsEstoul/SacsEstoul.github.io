003e192f28b36d87b01652d2560238f384146a37
Sacs Estoul <96799620+SacsEstoul@users.noreply.github.com>
2022-03-15 16:02:50 -0700

    Update firstPhoto.js

--- code/js/firstPhoto.js
+++ code/js/firstPhoto.js
@@ -68,7 +68,7 @@ class FirstPhoto extends HTMLElement {
          margin-bottom: 2cm;
          overflow: hidden; 	
     }    
-    
+    <p>${addressHTML}</p>
     <div class="col-md-4 portfolio-item">
     
     <p style="font-size:25px;text-align:center;font-family:Optima"><a href=${addressHTML} id=${nomNum}></a></p>        
