676edc534fc356f1eaecc6504f9b0a3d907b3201
Sacs Estoul <96799620+SacsEstoul@users.noreply.github.com>
2022-03-27 17:21:47 -0700

    Update firstPhoto.js

--- code/js/firstPhoto.js
+++ code/js/firstPhoto.js
@@ -5,70 +5,40 @@ class FirstPhoto extends HTMLElement {
 
   connectedCallback() {
 //     // should be a string, "1", "2", "3", etc.
-//     var numeroSac = this.attributes.numero.value;
-//     // should be a string, "sacs_1_9", "sacs_10_18", etc.
-//     var pageSac = this.attributes.page.value;
-//     // temporary, until all pageSac are the same for photos and pages suivantes	  
-//     var pagePhoto = this.attributes.page_photo.value;
+       var numSac = this.attributes.numero.value;
+       var nomSac = "sac" + numSac;
+       var prixSac = "prix" + numSac;	  
 	  
-//     // "true" or "false" (str), is it on the first page (index.html) or one of the following ones (onglet#.html).
-//     // Used to know if we need "../" in link
-//     var isLanding = this.attributes.landing_page.value;
-    
-//     var strSac = "sac" + numeroSac;
-    
-//     var nomNum = "nom" + numeroSac;
-//     var prixNum = "prix" + numeroSac;
-    
-//     if (isLanding == "true") {
-//       var addressHTML = "sacs_details/" + pageSac + "/" + strSac + ".html";
-//       var addressJPG = "sacs_details/" + pagePhoto + "/" + strSac + ".JPG";  
-//     }  
-//     else {
-//       var addressHTML = "../sacs_details/" + pageSac + "/" + strSac + ".html";  
-//       var addressJPG = "../sacs_details/" + pagePhoto + "/" + strSac + ".JPG";
-//     }  
-//     var addressHTML = "sacs_details/sacs_1_9/sac1.html" 
-//     var addressJPG = "sacs_details/sacs_1-9/sac1.JPG"  
-    
-    
-    
-
-
-//     fetch('ESTELLE/info_sacs.json')
-//     .then(res => res.json())
-//     .then((res) => {
-//     var donnees = res.données_sacs;
-//     var infoSac = donnees["sac1"];
-      
-//     var nomSac = infoSac.nom;
-//     var prixSac = infoSac.prix;
-	  
-
-
- 	       
-//    });	  
-	  
+       fetch('ESTELLE/info_sacs.json')
+       .then(res => res.json())
+       .then((res) => {
+       var data = res.données_sacs;  
+       var sac = data[nomSac];
+       var nom = sac.nom;
+       var prix = sac.prix;	       
+       });  
     
     
     this.innerHTML = `
+    <style>
+    .bottom-one {
+         margin-bottom: 1cm;
+         overflow: hidden; 
+    }
+    .bottom-two {
+         margin-bottom: 2cm;
+         overflow: hidden; 	
+    }
+    </style>
     
-   <h3 id="prix1"></h3>
-       
-   <script>    
-   
-   function getElement(id) {
-       return document.getElementById(id);
-   }	  
-      
-
-   var prixSac = "10";  
-      
-   // Prix: 	   
-   getElement("prix1").innerHTML = prixSac;  
-   
-   
-   </script>
+    <div class="col-md-4 portfolio-item">
+    <p style="font-size:25px;text-align:center;font-family:Optima"><a href="sacs_details/sacs_1_9/sac1.html">${nom}</a></p>   
+    <a href="sacs_details/sacs_1_9/sac1.html">
+       <img class="img-responsive" style="text-align:center;width:350px;height:525px;" src="photos/sacs_1-9/sac1.JPG" onerror="this.style.visibility = 'hidden'"></img>
+    </a>
+    <h3>prix</h3> 	                               
+    </div>	
+    
 	
     `;
   }
